7e83673459f3a9ea97f738a0fe6ecb5c0c3530ed
Sugatraj <sugatraj.2106@gmail.com>
2025-05-23 13:14:42 +0530

    MIGRATION TO OWNER APP CODEBASE

--- components/MaterialIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MaterialIcons } from "@expo/vector-icons";
-
-const MaterialIcon = ({ name, size = 24, color = "#000", style }) => {
-  return <MaterialIcons name={name} size={size} color={color} style={style} />;
-};
-
-export default MaterialIcon;
--- utils/themeHelpers.js
+++
@@ -1,9 +0,0 @@
-import { useColorMode } from 'native-base';
-
-export function useThemeColor(lightValue, darkValue) {
-  const { colorMode } = useColorMode();
-  return colorMode === 'dark' ? darkValue : lightValue;
-}
-
-// Usage in components:
-// const backgroundColor = useThemeColor('white', 'coolGray.900'); 
